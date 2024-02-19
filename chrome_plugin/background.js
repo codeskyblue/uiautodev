b9a35b25a7df2889dea739d156148e93832a0763
codeskyblue <codeskyblue@gmail.com>
2024-02-19 15:08:12 +0800

    update chrome plugin

--- chrome_plugin/background.js
+++ chrome_plugin/background.js
@@ -1,36 +1,50 @@
-// import { D } from "./d.js";
-
 (function mainBackground() {
-    // D.func();
-    // const response = await fetch('https://www.example.com/greeting.json')
-    // console.log(response.statusText);
     console.log("mainBackground")
     const port = 20242;
 
+    function _arrayBufferToBase64(buffer) {
+        var binary = '';
+        var bytes = new Uint8Array(buffer);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return btoa(binary);
+    }
+
+    async function fetchByMessage(message) {
+        const response = await fetch(`http://localhost:${port}/${message.url.replace(/^\//, '')}`, {
+            method: message.method || "GET",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: message.body && JSON.stringify(message.body)
+        });
+        if (response.headers.get("content-type") == "application/json") {
+            return response.json();
+        } else {
+            return response.arrayBuffer().then(buffer => {
+                return _arrayBufferToBase64(buffer);
+            });
+        }
+    }
 
     chrome.runtime.onInstalled.addListener(async () => {
         chrome.storage.sync.set({ port });
         console.log(`[AppInspector] default client port is set to: ${port}`);
         try {
-            const response = await fetch(`http://localhost:${port}/info`);
-            response.json().then(data => {
-                console.log(data);
-            });
+            const data = await fetchByMessage({ url: "/info" });
+            console.log(JSON.stringify(data));
         } catch (error) {
             console.log("error:", error)
         }
     });
 
-
     chrome.runtime.onMessageExternal.addListener(async (message, sender, callback) => {
-        console.log(JSON.stringify(message))
         try {
-            const response = await fetch(`http://localhost:${port}/${message.url.replace(/^\//, '')}`, {
-                method: message.method || "GET"
-            });
-            response.json().then(data => {
-                callback({ error: null, data })
-            });
+            const data = await fetchByMessage(message)
+            callback({ error: null, data })
         } catch (error) {
             callback({ error: error + "" })
         }
--- chrome_plugin/popup.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onMessageExternal.addListener(function onMessageExternal(message, sender, callback) {
-    console.log("popup", message, sender, callback);
-    callback("popup response");
-})
